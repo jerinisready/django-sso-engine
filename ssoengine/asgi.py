bc848263b0eb923248b82b337c2c14860de18943
Jerin.John <jjohn@payfare.com>
2023-10-25 23:11:55 +0530

    updated project name

--- ssoengine/asgi.py
+++ ssoengine/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djsso project.
+ASGI config for ssoengine project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djsso.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssoengine.settings')
 
 application = get_asgi_application()
--- ssoengine/wsgi.py
+++ ssoengine/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djsso project.
+WSGI config for ssoengine project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djsso.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssoengine.settings')
 
 application = get_wsgi_application()
